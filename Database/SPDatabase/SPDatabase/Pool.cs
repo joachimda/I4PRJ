9cfdb1fb2e688695c2fdc5030837b7d5a5c44f54
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-03-02 16:01:13 +0100

    refactoring pool class, not done yet

--- Database/SPDatabase/SPDatabase/Pool.cs
+++ Database/SPDatabase/SPDatabase/Pool.cs
@@ -1,10 +1,34 @@
+using System.Collections.Generic;
+
 namespace SPDatabase
 {
     public class Pool
     {
+        #region Properties
+
         public int PoolId { get; set; }
-        public int Depth { get; set; }
-        public int Length { get; set; }
-        public int Width { get; set; }
+
+        private List<MonitorUnit> _monitorUnits;
+
+        public List<MonitorUnit> MonitorUnits
+        {
+            get { return _monitorUnits; }
+            set { _monitorUnits = value; }
+        }
+
+        private double _averageTemperature;
+
+        public double AverageTemperature
+        {
+            get { return _averageTemperature; }
+            set { _averageTemperature = value; }
+        }
+
+
+        #endregion
+
+        //public int Depth { get; set; }
+        //public int Length { get; set; }
+        //public int Width { get; set; }
     }
 }
