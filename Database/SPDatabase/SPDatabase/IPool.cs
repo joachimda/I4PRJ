0023c7005ef57dd15025ba0fb3b8e4a9bde47b8a
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-03-02 16:05:16 +0100

    extracted interface for pool and refactoring should be done for now

---
+++ Database/SPDatabase/SPDatabase/IPool.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SPDatabase
+{
+    public interface IPool
+    {
+        double AverageTemperature { get; set; }
+        List<MonitorUnit> MonitorUnits { get; set; }
+        int PoolId { get; set; }
+    }
+}
--- Database/SPDatabase/SPDatabase/Pool.cs
+++ Database/SPDatabase/SPDatabase/Pool.cs
@@ -2,33 +2,20 @@ using System.Collections.Generic;
 
 namespace SPDatabase
 {
-    public class Pool
+    public class Pool : IPool
     {
         #region Properties
 
         public int PoolId { get; set; }
-
-        private List<MonitorUnit> _monitorUnits;
-
-        public List<MonitorUnit> MonitorUnits
-        {
-            get { return _monitorUnits; }
-            set { _monitorUnits = value; }
-        }
-
-        private double _averageTemperature;
-
-        public double AverageTemperature
-        {
-            get { return _averageTemperature; }
-            set { _averageTemperature = value; }
-        }
-
-
-        #endregion
+        public List<MonitorUnit> MonitorUnits { get; set; }
+        public double AverageTemperature { get; set; }
 
         //public int Depth { get; set; }
         //public int Length { get; set; }
         //public int Width { get; set; }
+
+        #endregion
+
+
     }
 }
