949a427da682aa867010a85d5b8d4a632359954c
Joachim Andersen <joachimdamandersen@gmail.com>
2016-03-03 19:58:08 +0100

    Realname now cant be less than 3 characters

--- Database/SPDatabase/SPDatabase/Visualizer.cs
+++ Database/SPDatabase/SPDatabase/Visualizer.cs
@@ -9,44 +9,37 @@ namespace SPDatabase
         {
             RealName name = new RealName();
             Formatter formatter = new Formatter();
+            bool validNameEntered = false;
 
             Console.WriteLine("You are now adding a user to the SmartPool database ");
             Console.WriteLine("Please enter your full name (Use a maximum of 3 names), then press ENTER ");
-            string fullName = Console.ReadLine();
 
             string[] splitNames = {};
 
-            if (fullName != null)
+            while (!validNameEntered)
             {
-                splitNames = fullName.Split(' ');
-                Console.WriteLine("You have input: ");
-                foreach (var partOfName in splitNames)
+                string fullName = Console.ReadLine();
+
+                if (fullName != null && fullName.Length > 3)
                 {
-                    Console.Write(" {0}", partOfName);
+                    splitNames = fullName.Split(' ');
+                    Console.WriteLine("You have input: ");
+                    foreach (var partOfName in splitNames)
+                    {
+                        Console.Write(" {0}", partOfName);
+                    }
+                    validNameEntered = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Please write a valid name ");
                 }
             }
 
             return formatter.FormatRealNameInputFromStringArray(splitNames, name);
 
-            /*
-            switch (splitNames.Length)
-            {
-                case 1:
-                    name.FirstName = splitNames[0];
-                    break;
-                case 2:
-                    name.FirstName = splitNames[0];
-                    name.SurName = splitNames[1];
-                    break;
-                case 3:
-                    name.FirstName = splitNames[0];
-                    name.MiddleName = splitNames[1];
-                    name.SurName = splitNames[2];
-                    break;
             }
-            return name;
-            */
-        }
 
         public void DrawPossibleCommands()
         {
@@ -58,29 +51,6 @@ namespace SPDatabase
             Console.WriteLine(String.Format("|Print name query  |qry                |"));
             Console.WriteLine(String.Format("|Quit              |NOT SPECIFIED      |"));
             Console.WriteLine(String.Format("|******************|*******************|"));
-        }
-    }
-
-    public class Formatter
-    {
-        public RealName FormatRealNameInputFromStringArray(string[] nameStrings, RealName name)
-        {
-            switch (nameStrings.Length)
-            {
-                case 1:
-                    name.FirstName = nameStrings[0];
-                    break;
-                case 2:
-                    name.FirstName = nameStrings[0];
-                    name.SurName = nameStrings[1];
-                    break;
-                case 3:
-                    name.FirstName = nameStrings[0];
-                    name.MiddleName = nameStrings[1];
-                    name.SurName = nameStrings[2];
-                    break;
-            }
-            return name;
         }
     }
 }
