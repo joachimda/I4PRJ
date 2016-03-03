9ac5a851b5ae7d92be48149a7b6dd68b97edd875
Joachim Andersen <joachimdamandersen@gmail.com>
2016-03-03 19:42:32 +0100

    Found a way to update tables. Formatter class moved to own file

---
+++ Database/SPDatabase/SPDatabase/Migrations/201603031838020_tests1.cs
@@ -0,0 +1,16 @@
+namespace SPDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tests1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
