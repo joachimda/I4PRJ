047ca04eea8f7eaff0f0cde091fa990110e961a7
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-03-31 08:28:14 +0200

    initial commit

---
+++ Database/CreditcardValidation/CreditcardValidationTest/CreditcardValidationTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CreditcardValidationTest
+{
+    [TestFixture]
+    public class CreditcardValidationTest
+    {
+        private CreditcardValidation.CreditcardValidation _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new CreditcardValidation.CreditcardValidation();
+        }
+    }
+}
