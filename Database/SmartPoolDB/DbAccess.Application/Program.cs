0bd8de85805e9b1e5f093844cc6b935e58609783
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-04-01 11:42:54 +0200

    ...

--- Database/SmartPoolDB/DbAccess.Application/Program.cs
+++ Database/SmartPoolDB/DbAccess.Application/Program.cs
@@ -10,8 +10,6 @@
             dbAccess.UserAccess.AddUser("Lars", "Peter", "Jensen", "lars@jensen.com", "larssPassword");
             dbAccess.UserAccess.AddUser("Signe", "Jensen", "signe@jensen.com", "signesPassword");
             dbAccess.UserAccess.AddUser("Nanna", "Petersen", "nanna@petersen.com", "nannasPassword");
-            dbAccess.UserAccess.AddUser("John", "Derp", "john@derp.com", "herpderp");
-
 
             //dbAccess.UserAccess.RemoveUser("peter@jensen.com");
             //dbAccess.PoolAccess.AddPool(dbAccess.UserAccess.FindUserByEmail("peter@jensen.com").ElementAt(0), "Baghave", 3, 4, 2);
