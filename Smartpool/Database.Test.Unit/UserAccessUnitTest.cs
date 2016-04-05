327a86864c1ee819b7e45c9debf67e3b04fc83c7
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-04-05 22:28:31 +0200

    dunno

---
+++ Smartpool/Database.Test.Unit/UserAccessUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Smartpool.UserAccess;
+
+namespace Database.Test.Unit
+{
+    [TestFixture]
+    public class UserAccessUnitTest
+    {
+        #region Setup
+
+        IUserAccess _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new UserAccess();
+        }
+
+        #endregion
+
+        [Test]
+        public void AddUser_DoesNotAddUser_NUnitIsWorking()
+        {
+            Assert.That(5, Is.EqualTo(5));
+        }
+    }
+}
