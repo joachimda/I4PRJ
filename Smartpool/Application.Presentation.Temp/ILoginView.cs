ae5057403ef16e6d1d354d4823ee66652b6322ab
Emil <emil-ny@hotmail.com>
2016-04-07 17:49:27 +0200

    Presenter temp and Design spike complete. SpButton can be disabled

---
+++ Smartpool/Application.Presentation.Temp/ILoginView.cs
@@ -0,0 +1,39 @@
+﻿//========================================================================
+// FILENAME :   ILoginView.cs
+// DESCR.   :   Interface for login views
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  LP      Initial version
+//========================================================================
+
+// ReSharper disable once CheckNamespace
+namespace Smartpool.Application.Presentation.Temp
+{
+    public interface ILoginView : IView
+    {
+		/// <summary>
+		/// Sets the text of the email text field
+		/// </summary>
+        void SetEmailText(string text);
+
+		/// <summary>
+		/// Sets the text of the password text field
+		/// </summary>
+        void SetPasswordText(string text);
+
+		/// <summary>
+		/// Sets the state of the login button
+		/// </summary>
+        void SetLoginButtonEnabled(bool enabled);
+
+		/// <summary>
+		/// Displays a message or alert on the view
+		/// </summary>
+        void DisplayAlert(string title, string content);
+
+		/// <summary>
+		/// Tells the view that a login request has been accepted, should present the main menu
+		/// </summary>
+		void LoginAccepted();
+    }
+}
---
+++ Smartpool/Application.Presentation.Temp/ILoginViewController.cs
@@ -0,0 +1,39 @@
+﻿//========================================================================
+// FILENAME :   ILoginViewController.cs
+// DESCR.   :   Interface for login view presenters
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  LP      Initial version
+//========================================================================
+
+// ReSharper disable once CheckNamespace
+namespace Smartpool.Application.Presentation.Temp
+{
+	/// <summary>
+	/// Enumeration of possible buttons on the Login View
+	/// </summary>
+    public enum LoginViewButton : int
+    {
+        LoginButton = 0,
+        SignUpButton = 1,
+        ForgotButton = 2
+    }
+
+    public interface ILoginViewController : IViewController
+    {
+		/// <summary>
+		/// Called by the Login View when a button is pressed
+		/// </summary>
+        void ButtonPressed(LoginViewButton button);
+
+		/// <summary>
+		/// Called by the Login View when the email text field changed
+		/// </summary>
+        void DidChangeEmailText(string text);
+
+		/// <summary>
+		/// Called by the Login View when the password text field changed
+		/// </summary>
+        void DidChangePasswordText(string text);
+    }
+}
