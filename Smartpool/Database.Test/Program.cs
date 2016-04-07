683e21f5439ec9eebd00046d7cafa56c9ca22a01
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-04-07 15:04:26 +0200

    ...

--- Smartpool/Database.Test/Program.cs
+++ Smartpool/Database.Test/Program.cs
@@ -8,13 +8,13 @@ namespace Database.Test
         {
             Smartpool.SmartpoolDB SmartPoolDB = new Smartpool.SmartpoolDB(new StdAccessFactory());
 
-            //SmartPoolDB.UserAccess.DeleteAllUsers();
+            SmartPoolDB.UserAccess.DeleteAllUsers();
             SmartPoolDB.UserAccess.AddUser("Joachim Dam Andersen", "jokke@norgaard-andersen.tech", "herropree");
             SmartPoolDB.UserAccess.AddUser("Bjørn Nørgaaard Sørensen", "bjorn@norgaard-andersen.tech", "helloyou");
             SmartPoolDB.UserAccess.AddUser("Signe Satan", "signe@hotmail.com", "signespassword");
             SmartPoolDB.UserAccess.AddUser("Nanna Derps", "derp@gmail.com", "monstersikkerdk");
 
-            SmartPoolDB.UserAccess.RemoveUser("jokke@norgaard-andersen.tech");
+            //SmartPoolDB.UserAccess.RemoveUser("jokke@norgaard-andersen.tech");
         }
     }
 }
