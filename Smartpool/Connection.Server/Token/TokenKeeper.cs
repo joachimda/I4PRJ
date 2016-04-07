0dc600fff76312d7d962669d1b163a009efcb883
Wind2dk <Wind2dk@gmail.com>
2016-04-08 01:30:05 +0200

    Added token files

---
+++ Smartpool/Connection.Server/Token/TokenKeeper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ServerTest.Token
+{
+    public class TokenKeeper
+    {
+        private readonly List<IToken> _tokens = new List<IToken>();
+        private readonly ITokenStringGenerator _tokenStringGenerator;
+
+        public TokenKeeper(ITokenStringGenerator tokenStringGenerator)
+        {
+            _tokenStringGenerator = tokenStringGenerator;
+        }
+
+        public bool TokenActive(string username, string tokenString)
+        {
+            foreach (var token in _tokens)
+            {
+                if (token.GetTokenOwner() == username && token.GetTokenString() == tokenString)
+                {
+                    if (token.TokenAlive())
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public void CreateNewToken(string username)
+        {
+            CheckForOldToken(username);
+            _tokens.Add(new Token(username, _tokenStringGenerator));
+        }
+
+        private void CheckForOldToken(string username)
+        {
+            foreach (var token in _tokens)
+            {
+                if (token.GetTokenOwner() == username)
+                {
+                    _tokens.Remove(token);
+                    return;
+                }
+            }
+        }
+    }
+}
