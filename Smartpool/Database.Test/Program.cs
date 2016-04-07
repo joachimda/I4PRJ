00a99d7f15d25c03fb6a4c822df1281745a8cbfe
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-04-07 17:30:33 +0200

    ...

--- Smartpool/Database.Test/Program.cs
+++ Smartpool/Database.Test/Program.cs
@@ -8,11 +8,12 @@ namespace Database.Test
         {
             Smartpool.SmartpoolDB SmartPoolDB = new Smartpool.SmartpoolDB(new StdAccessFactory());
 
-            SmartPoolDB.UserAccess.DeleteAllUsers();
-            SmartPoolDB.UserAccess.AddUser("Joachim Dam Andersen", "jokke@norgaard-andersen.tech", "herropree");
-            SmartPoolDB.UserAccess.AddUser("Bjørn Nørgaaard Sørensen", "bjorn@norgaard-andersen.tech", "helloyou");
-            SmartPoolDB.UserAccess.AddUser("Signe Satan", "signe@hotmail.com", "signespassword");
-            SmartPoolDB.UserAccess.AddUser("Nanna Derps", "derp@gmail.com", "monstersikkerdk");
+            //SmartPoolDB.UserAccess.DeleteAllUsers();
+            //SmartPoolDB.UserAccess.AddUser("Joachim Dam Andersen", "jokke@norgaard-andersen.tech", "herropree");
+            //SmartPoolDB.UserAccess.AddUser("Bjørn Nørgaaard Sørensen", "bjorn@norgaard-andersen.tech", "helloyou");
+            //SmartPoolDB.UserAccess.AddUser("Signe Satan", "signe@hotmail.com", "signespassword");
+            //SmartPoolDB.UserAccess.AddUser("Nanna Derps", "derp@gmail.com", "monstersikkerdk");
+            SmartPoolDB.UserAccess.AddUser("Emil Lasse", "emil@lasse.com", "pikpikpik");
 
             //SmartPoolDB.UserAccess.RemoveUser("jokke@norgaard-andersen.tech");
         }
