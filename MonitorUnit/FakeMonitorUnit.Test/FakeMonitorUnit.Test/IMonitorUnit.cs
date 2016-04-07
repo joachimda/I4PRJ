bf88f64d0a2499ac4ec2acc121cd1750492b025e
Joachim Andersen <joachimdamandersen@gmail.com>
2016-04-07 15:20:27 +0200

    implemeted FakeMonitorUnit and all get methods

--- MonitorUnit/FakeMonitorUnit.Test/FakeMonitorUnit.Test/IMonitorUnit.cs
+++ MonitorUnit/FakeMonitorUnit.Test/FakeMonitorUnit.Test/IMonitorUnit.cs
@@ -12,4 +12,29 @@ namespace FakeMonitorUnit.Test
         double GetChlorineData();
         double GetHumidityData();
     }
+
+    class FakeMonitorUnit : IMonitorUnit
+    {
+        public double GetPhData()
+        {
+            Random rnd = new Random();
+
+            var val = rnd.Next(1, 14);
+            return val;
+        }
+
+        public double GetChlorineData()
+        {
+            Random rnd = new Random();
+            var val = rnd.Next(100, 1000);
+            return val;
+        }
+
+        public double GetHumidityData()
+        {
+            Random rnd = new Random();
+            var val = rnd.Next(0, 100);
+            return val;
+        }
+    }
 }
--- MonitorUnit/FakeMonitorUnit.Test/FakeMonitorUnit.Test/Program.cs
+++ MonitorUnit/FakeMonitorUnit.Test/FakeMonitorUnit.Test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FakeMonitorUnit.Test
@@ -10,6 +11,16 @@ namespace FakeMonitorUnit.Test
     {
         static void Main(string[] args)
         {
+            IMonitorUnit monitor = new FakeMonitorUnit();
+
+            while (true)
+            {
+                Console.WriteLine("Chlorine level is " + monitor.GetChlorineData() + " units");
+                Console.WriteLine("Room humidity is " + monitor.GetHumidityData() + "%");
+                Console.WriteLine("pH level is " + monitor.GetPhData());
+                Thread.Sleep(1000);
+
+            }
 
         }
     }
