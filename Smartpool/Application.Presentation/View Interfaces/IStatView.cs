51038bcb322b158d7e288f3eb32a92043c1a6f61
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-04-07 15:01:13 +0200

    Added stat view and controller (unfinished implementation)

---
+++ Smartpool/Application.Presentation/View Interfaces/IStatView.cs
@@ -0,0 +1,15 @@
+﻿//========================================================================
+// FILENAME :   IStatView.cs
+// DESCR.   :   Interface for stat views
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 0.1  LP      Initial version, missing implementation
+//========================================================================
+
+// ReSharper disable once CheckNamespace
+namespace Smartpool.Application.Presentation
+{
+	public interface IStatView : IView
+	{
+	}
+}
