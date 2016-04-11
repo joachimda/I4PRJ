c109d0d8560d554555ceb929287d498775fb16dd
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-04-11 11:32:25 +0200

    Sign up interfaces and presenter implemented

---
+++ Smartpool/Application.Presentation/Presenters/ISignUpViewController.cs
@@ -0,0 +1,34 @@
+﻿//========================================================================
+// FILENAME :   ISignUpViewController.cs
+// DESCR.   :   Interface for sign up view presenters
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  LP      Initial version
+//========================================================================
+
+// ReSharper disable once CheckNamespace
+namespace Smartpool.Application.Presentation
+{
+    public interface ISignUpViewController : IViewController
+    {
+        /// <summary>
+        /// Called by the Sign Up View when the sign up button is pressed
+        /// </summary>
+        void ButtonPressed();
+
+        /// <summary>
+        /// Called by the Sign Up View when the name text field changed
+        /// </summary>
+        void DidChangeNameText(string text);
+
+        /// <summary>
+        /// Called by the Sign Up View when the email text field changed
+        /// </summary>
+        void DidChangeEmailText(string text);
+
+        /// <summary>
+        /// Called by the Sign Up View when either of the the password text fields changed
+        /// </summary>
+        void DidChangePasswordText(string text, int fieldNumber);
+    }
+}
---
+++ Smartpool/Application.Presentation/Presenters/SignUpViewController.cs
@@ -0,0 +1,121 @@
+﻿//========================================================================
+// FILENAME :   SignUpViewController.cs
+// DESCR.   :   Default implementation of the sign up view presenter
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  LP      Initial version
+//========================================================================
+
+using System;
+using Smartpool.Application.Model;
+
+// ReSharper disable once CheckNamespace
+namespace Smartpool.Application.Presentation
+{
+    public class SignUpViewController : ISignUpViewController
+    {
+        // Properties
+
+        private readonly IClient _client; // temporary, needs a real IClient
+        private readonly ISignUpView _view;
+        private string _name = "";
+        private string[] _passwords = {"", ""};
+        private string _email = "";
+        private bool _passwordValid = false;
+
+        // Life Cycle
+        public void ViewDidLoad()
+        {
+            // Reset text boxes
+            _view.SetNameText("");
+            _view.SetEmailText("");
+            _view.SetPasswordText("");
+
+            // Disable login button
+            _view.SetPasswordValid(false);
+            _view.SetButtonEnabled(false);
+        }
+
+        public SignUpViewController(ISignUpView view, IClient client = null)
+        {
+            _view = view;
+            _client = client;
+        }
+
+        // Interface
+
+        public void ButtonPressed()
+        {
+            SignUp();
+        }
+
+        public void DidChangeNameText(string text)
+        {
+            _name = text;
+            UpdateSignUpButton();
+        }
+
+        public void DidChangeEmailText(string text)
+        {
+            _email = text;
+            UpdateSignUpButton();
+        }
+
+        public void DidChangePasswordText(string text, int fieldNumber)
+        {
+            if (fieldNumber > 1) throw new ArgumentException();
+
+            // Store the password text
+            _passwords[fieldNumber] = text;
+
+            UpdatePassword();
+            UpdateSignUpButton();
+        }
+
+        // LoginViewController
+
+        public void UpdatePassword()
+        {
+            var password = _passwords[0];
+            var minimumCharacters = 6;
+            if (password == _passwords[1] && password.Length >= minimumCharacters)
+            {
+                _passwordValid = true;
+            }
+            else
+            {
+                _passwordValid = false;
+            }
+            _view.SetPasswordValid(_passwordValid);
+        }
+
+        public void UpdateSignUpButton()
+        {
+            // Enable button if user entered password, name and email
+            if (_passwordValid && _email.Length > 0 && _name.Length > 0)
+            {
+                _view.SetButtonEnabled(true);
+            }
+            else {
+                _view.SetButtonEnabled(false);
+            }
+        }
+
+        public void SignUp()
+        {
+            // Create a new sign up command
+            var clientCommand = new ClientCommands(_client);
+
+            // Missing implementation
+            var successful = false;
+            if (successful)
+            {
+                _view.SignUpAccepted();
+            }
+            else
+            {
+                _view.DisplayAlert("Invalid Sign Up", "The e-mail is already used");
+            }
+        }
+    }
+}
