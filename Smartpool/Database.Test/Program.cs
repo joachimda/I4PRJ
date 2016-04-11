03c141a53fb1e957b295564a2a562035af2f570b
Joachim Andersen <joachimdamandersen@gmail.com>
2016-04-11 12:28:12 +0200

    Added comments on using the configuration manager

--- Smartpool/Database.Test/Program.cs
+++ Smartpool/Database.Test/Program.cs
@@ -1,4 +1,8 @@
-﻿using Smartpool.Factories;
+﻿using System;
+using System.Data.Entity.Infrastructure.Design;
+using Smartpool.Factories;
+using System.Configuration;
+
 
 namespace Database.Test
 {
@@ -16,8 +20,19 @@ namespace Database.Test
             SmartPoolDB.UserAccess.AddUser("Emil Lasse", "emil@lasse.com", "pikpikpik");
             SmartPoolDB.UserAccess.AddUser("lol derp lol", "hejza", "pissword");
             SmartPoolDB.UserAccess.AddUser("Cartman San", "Lelz@herp.so", "lort");
-
             //SmartPoolDB.UserAccess.RemoveUser("jokke@norgaard-andersen.tech");
         }
     }
+    /*****************************Don't touch diz!*******************************/
+    //var connectionStringCollection = System.Configuration.ConfigurationManager.ConnectionStrings;
+    //ConnectionStringSettings connectionStringSetter = new ConnectionStringSettings();
+    //connectionStringSetter.ConnectionString = "BjornIsNoob";
+    //System.Configuration.ConfigurationManager.ConnectionStrings.Add(connectionStringSetter);
+    //Console.WriteLine(connectionStringCollection.Count);
+    //var appConfigs = ConfigurationManager.AppSettings;
+    //var c = connectionStringCollection.Count;
+    //foreach (var key in appConfigs.AllKeys)
+    //{
+    //    Console.WriteLine("Key: {0} Value: {1}", key, appConfigs[key]);
+    //}
 }
