8d74b583590c782dfd1921f9fdb3d7cde4d5a135
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-04-28 13:24:28 +0200

    updated interface

--- Smartpool/Database/PoolAccess/IPoolAccess.cs
+++ Smartpool/Database/PoolAccess/IPoolAccess.cs
@@ -2,10 +2,10 @@
 {
     public interface IPoolAccess
     {
-        void AddPool(User user, string address, string name, double volume);
-        bool IsPoolNameInUse(User user, string address, string name);
-        Pool FindSpecificPool(User user, string address, string name);
-        void RemovePool(User user, string address, string name);
+        void AddPool(User user, string name, double volume);
+        bool IsPoolNameInUse(User user, string name);
+        Pool FindSpecificPool(User user, string name);
+        void RemovePool(User user, string name);
         void DeleteAllPools();
     }
 }
