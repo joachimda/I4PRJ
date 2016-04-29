32cfc1d5e0b10ac7388c3f5d8d122af7ddd5e305
Emil <emil-ny@hotmail.com>
2016-04-29 16:32:40 +0200

    Added AddPool and EditPool to SpTabControl. Added history comment in top of all .cs files

--- Smartpool/Application.Win/WinLoginView.xaml.cs
+++ Smartpool/Application.Win/WinLoginView.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Windows;
+﻿//========================================================================
+// DESCR.   :   Codebehind that calls the presenter.
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  EN      Initial version with event for Stats
+// 1.01 EN      Added event for History
+// 1.02 EN      
+//========================================================================
+
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
--- Smartpool/Application.Win/WinStatView.xaml.cs
+++ Smartpool/Application.Win/WinStatView.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Windows;
+﻿//========================================================================
+// DESCR.   :   Codebehind that calls the presenter.
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  EN      Initial version with event for Stats
+// 1.01 EN      Added event for History
+// 1.02 EN      
+//========================================================================
+
+using System.Windows;
 
 // ReSharper disable once CheckNamespace
 namespace Smartpool.Application.Win
@@ -12,13 +21,30 @@ namespace Smartpool.Application.Win
         {
             InitializeComponent();
 
-            SpTabControl1.OnShowStatButtonClicked += SpTabControl1_OnClickMeClicked;
-            SpTabControl1.OnShowHistoryButtonClicked += SpTabControl1_OnClickMeClicked;
+            SpTabControl1.OnShowStatButtonClicked += SpTabControl1_ShowStatView;
+            SpTabControl1.OnShowHistoryButtonClicked += SpTabControl1_ShowHistoryView;
+            SpTabControl1.OnShowAddPoolButtonClicked += SpTabControl1_ShowAddPoolView;
+            SpTabControl1.OnShowEditPoolButtonClicked += SpTabControl1_ShowEditPoolView;
         }
 
-        private void SpTabControl1_OnClickMeClicked(object sender, RoutedEventArgs e)
+        private void SpTabControl1_ShowStatView(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Show Stats or History");
+            MessageBox.Show("Show Stats");
+        }
+
+        private void SpTabControl1_ShowHistoryView(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Show History");
+        }
+
+        private void SpTabControl1_ShowAddPoolView(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Show AddPool");
+        }
+
+        private void SpTabControl1_ShowEditPoolView(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Show EditPool");
         }
     }
 }
