5938ba9be890dd1e688cf330874d3a32ab11fc5e
Alex Karlsen <alexkarlsen7@gmail.com>
2016-05-02 11:02:14 +0200

    Smartpool website using ASP.Net

---
+++ Smartpool_Website/Smartpool_Website/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Smartpool_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login Page";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult StatView()
+        {
+            ViewBag.Message = "StatView page";
+
+            return View();
+        }
+
+    }
+}
---
+++ Smartpool_Website/Smartpool_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Smartpool_Website.Startup))]
+namespace Smartpool_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
