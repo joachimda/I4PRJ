5b616ed32d89c4c38ae463cf45cc6aa48d9eafce
Wind2dk <Wind2dk@gmail.com>
2016-05-04 00:40:39 +0200

    Created fakePoolDataGeneration

---
+++ Smartpool/Connection.Server/FakePoolDataGeneration/FakePool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Timers;
+
+namespace Smartpool.Connection.Server.FakePoolDataGeneration
+{
+    public class FakePool
+    {
+        private readonly int _amountOfSensors;
+        private readonly List<FakeSensor> _fakeSensors;
+
+        public FakePool(int amountOfSensors, int secondsBetweenSensorReadings)
+        {
+            _amountOfSensors = amountOfSensors;
+            _fakeSensors = new List<FakeSensor>();
+            GenerateSensors();
+            var timer = new Timer { Interval = 1000 * secondsBetweenSensorReadings };
+            timer.Elapsed += SaveSensorValue;
+            timer.Start();
+        }
+
+        private void GenerateSensors()
+        {
+            for (int i = 0; i < _amountOfSensors; i++)
+            {
+                _fakeSensors.Add(new FakeSensor());
+                System.Threading.Thread.Sleep(20);
+            }
+        }
+
+        private void SaveSensorValue(object sender, ElapsedEventArgs e)
+        {
+            foreach (var sensor in _fakeSensors)
+            {
+                sensor.GetNextSensorValue();
+                sensor.SaveValueToDatabase();
+            }
+        }
+    }
+}
---
+++ Smartpool/Connection.Server/FakePoolDataGeneration/FakeSensor.cs
@@ -0,0 +1,67 @@
+using System;
+
+namespace Smartpool.Connection.Server.FakePoolDataGeneration
+{
+    internal class FakeSensor
+    {
+        private readonly Random _random = new Random();
+        public FakeSensorEnum SensorType { get; set; }
+        public double SensorValue { get; set; }
+
+        public FakeSensor()
+        {
+            SensorType = (FakeSensorEnum)_random.Next(0, Enum.GetNames(typeof(FakeSensorEnum)).Length);
+            SensorValue = GetRandomSensorValue();
+        }
+        private double GetRandomSensorValue()
+        {
+            switch (SensorType)
+            {
+                case FakeSensorEnum.Temperature:
+                    return 25 + _random.Next(0, 11); //returns random value between 25 -> 35 with 1.0 precision
+
+                case FakeSensorEnum.Chlorine:
+                    return 0 + _random.Next(0, 21) * 0.1; //returns random value between 0 -> 2 with 0.1 precision
+
+                case FakeSensorEnum.Ph:
+                    return 7 + _random.Next(0, 9) * 0.1; //returns random value between 7 -> 7.8 with 0.1 precision
+
+                case FakeSensorEnum.Humidity:
+                    return 40 + _random.Next(0, 31); //returns random value between 40 -> 70 with 1.0 precision
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void GetNextSensorValue()
+        {
+            switch (SensorType)
+            {
+                case FakeSensorEnum.Temperature:
+                    SensorValue += _random.Next(-2, 3);
+                    break;
+
+                case FakeSensorEnum.Chlorine:
+                    SensorValue += _random.Next(-2, 3) * 0.1;
+                    break;
+
+                case FakeSensorEnum.Ph:
+                    SensorValue += _random.Next(-2, 3) * 0.1;
+                    break;
+
+                case FakeSensorEnum.Humidity:
+                    SensorValue += _random.Next(-2, 3);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void SaveValueToDatabase()
+        {
+            Console.WriteLine(DateTime.Now + " - Sensor of type: " + SensorType + " recorded a value of: " + SensorValue);
+        }
+    }
+}
