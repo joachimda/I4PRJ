e07d1be7d6721f2b2f010e31e61978d42fd7a3ec
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-05-04 16:29:15 +0200

    updated model to be 'prettier'

--- Smartpool/Database/Model.Context.cs
+++ Smartpool/Database/Model.Context.cs
@@ -7,9 +7,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System.Data.Entity.Validation;
-using System.Linq;
-
 namespace Smartpool
 {
     using System;
@@ -22,31 +19,7 @@ namespace Smartpool
             : base("name=DatabaseContext")
         {
         }
-
-        public override int SaveChanges()
-        {
-            try
-            {
-                return base.SaveChanges();
-            }
-            catch (DbEntityValidationException ex)
-            {
-                // Retrieve the error messages as a list of strings.
-                var errorMessages = ex.EntityValidationErrors
-                        .SelectMany(x => x.ValidationErrors)
-                        .Select(x => x.ErrorMessage);
-
-                // Join the list to a single string.
-                var fullErrorMessage = string.Join("; ", errorMessages);
-
-                // Combine the original exception message with the new one.
-                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
-
-                // Throw a new DbEntityValidationException with the improved exception message.
-                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
-            }
-        }
-
+    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
