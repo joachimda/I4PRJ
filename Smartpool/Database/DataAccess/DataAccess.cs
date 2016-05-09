9e1f73e44007665fd97228372d7a27beb83e8623
Joachim Derps <joachimdamandersen@gmail.com>
2016-05-09 13:59:04 +0200

    created temperature getter

--- Smartpool/Database/DataAccess/DataAccess.cs
+++ Smartpool/Database/DataAccess/DataAccess.cs
@@ -12,7 +12,7 @@ namespace Smartpool.DataAccess
         {
             PoolAccess = poolAccess;
         }
-        
+
         public bool CreateDataEntry(string ownerEmail, string poolName, double chlorine, double temp, double pH, double humidity)
         {
             throw new NotImplementedException();
@@ -41,7 +41,7 @@ namespace Smartpool.DataAccess
 
                 if (queryStartHour < 0)
                 {
-                    throw new ArgumentException("Invalid start hour, stupid!");
+                    throw new ArgumentException("Invalid starting hour from ChlorineQuery, stupid!");
                 }
 
                 #endregion
@@ -53,7 +53,7 @@ namespace Smartpool.DataAccess
                                         select chlorine;
                 #endregion
 
-                #region Add date-relevant chlorine date to Tuple list
+                #region Add date-relevant chlorine data to Tuple list
                 List<Tuple<long, double>> chlorineTuples = null;
 
                 foreach (var dataEntity in chlorineDataQuery)
@@ -71,7 +71,39 @@ namespace Smartpool.DataAccess
 
         public List<Tuple<long, double>> GetRecentTemperatureValues(string poolOwnerEmail, string poolName, long queryStartHour)
         {
-            throw new NotImplementedException();
+            using (var db = new DatabaseContext())
+            {
+                #region Check for invalid starting hour
+
+                if (queryStartHour < 0)
+                {
+                    throw new ArgumentException("Invalid starting hour from TemperatureQuery, stupid!");
+                }
+
+                #endregion
+
+                #region Query for all user-pool specific temperature data
+
+                var temperatureDataQuery = from temperature in db.TemperatureSet
+                                           where temperature.Data.Pool.Name == poolName && temperature.Data.Pool.User.Email == poolOwnerEmail
+                                           select temperature;
+                #endregion
+
+                #region Add date-relevant temperature data to Tuple list
+
+                List<Tuple<long, double>> temperatureTuples = null;
+
+                foreach (var dataEntity in temperatureDataQuery)
+                {
+                    if (dataEntity.Data.Timestamp > queryStartHour)
+                    {
+                        temperatureTuples.Add(new Tuple<long, double>(dataEntity.Data.Timestamp, dataEntity.Value));
+                    }
+                }
+                #endregion
+
+                return temperatureTuples;
+            }
         }
 
     }
