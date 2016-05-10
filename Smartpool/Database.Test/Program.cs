746fb2545c7f40053e865ed550d4c7674458abb5
Norgaard Tower <bjornsnorgaard@gmail.com>
2016-05-10 23:54:13 +0200

    ...

--- Smartpool/Database.Test/Program.cs
+++ Smartpool/Database.Test/Program.cs
@@ -13,13 +13,13 @@ namespace Database.Test
             SmartpoolDB SmartPoolDB = new SmartpoolDB(new PoolAccess(new UserAccess()));
 
             IWriteDataAccess dataAccess = new DataAccess();
-            dataAccess.PoolAccess = SmartPoolDB.PoolAccess;
-            dataAccess.DeleteAllData();
+            //dataAccess.PoolAccess = SmartPoolDB.PoolAccess;
+            //dataAccess.DeleteAllData();
 
-            SmartPoolDB.ClearEntireDatabase();
+            //SmartPoolDB.ClearEntireDatabase();
             
-            SmartPoolDB.UserAccess.AddUser("Sir john derp", "email", "pass");
-            SmartPoolDB.PoolAccess.AddPool("email", "baghave", 9);
+            //SmartPoolDB.UserAccess.AddUser("Sir john derp", "email", "pass");
+            //SmartPoolDB.PoolAccess.AddPool("email", "baghave", 9);
 
             dataAccess.CreateDataEntry("email", "baghave", 5, 9, 3, 10);
         }
