eba0fbd3e6e2ce80d543831a7a8d67bcf6fc46c4
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-15 20:55:24 +0200

    Added interface for edit user views

---
+++ Smartpool/Application.Presentation/View Interfaces/IEditUserView.cs
@@ -0,0 +1,29 @@
+﻿//========================================================================
+// FILENAME :   IEditUserView.cs
+// DESCR.   :   Interface for the edit user views
+//------------------------------------------------------------------------ 
+// REV. AUTHOR  CHANGE DESCRIPTION
+// 1.0  LP      Initial version
+//========================================================================
+
+// ReSharper disable once CheckNamespace
+namespace Smartpool.Application.Presentation
+{
+    public interface IEditUserView : IView, IAlertDisplaying
+    {
+        /// <summary>
+        /// Clears the text of all text fields
+        /// </summary>
+        void ClearAllText(string text);
+
+        /// <summary>
+        /// Sets the state of the save button (save password)
+        /// </summary>
+        void SetSaveButtonEnabled(bool enabled);
+
+        /// <summary>
+        /// Sets the state of password indicator of the new password
+        /// </summary>
+        void SetNewPasswordValid(bool valid);
+    }
+}
