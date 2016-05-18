e0a0a7b0a90eb8c85209f7cc5e5a57f1dfa72761
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-18 22:19:26 +0200

    iOS stat view done

---
+++ Smartpool/Application.iOS/GuiCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Smartpool.Connection.Model;
+
+namespace Application.iOS
+{
+	public class GuiCharacter
+	{
+		public static string SignForType(SensorTypes type)
+		{
+			switch (type)
+			{
+			case SensorTypes.Temperature:
+				return "°C";
+			case SensorTypes.Humidity:
+				return "%";
+			case SensorTypes.Chlorine:
+				return "ppm";
+			default:
+				return "";
+			}
+		}
+	}
+}
