43e9ae9f78a11f06d80de8a6647549c027c98a76
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2016-05-19 13:22:44 +0200

    iOS app final

---
+++ Smartpool/Application.iOS/IPViewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace Application.iOS
+{
+	public partial class IPViewController : UIViewController
+	{
+		public IPViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		partial void IPChanged (UIKit.UITextField sender)
+		{
+			iOSClientFactory.ServerIP = sender.Text;
+		}
+
+		public override void TouchesBegan (NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan (touches, evt);
+			View.EndEditing (true);
+		}
+	}
+}
